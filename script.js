77a24a5baab6e64bc47209f1068f968b62b937a2
gbelewEthiopia <130274874+gbelewEthiopia@users.noreply.github.com>
2023-04-09 03:20:24 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function updateTime() {
+    const container = document.getElementById('time');
+    const now = new Date();
+    let hours = now.getHours() + 1;
+    let timePeriod;
+  
+    if (hours >= 23 || hours < 5) {
+      timePeriod = "ጥዋት";
+    } else if (hours >= 5 && hours < 10) {
+      timePeriod = "ቀን";
+    } else if (hours >= 10 && hours < 16) {
+      timePeriod = "ምሽት";
+    } else if (hours >= 17 && hours < 23) {
+      timePeriod = "ሌሊት";
+    }
+  
+    let minutes = now.getMinutes();
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+  
+    let seconds = now.getSeconds();
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+  
+    const time = hours + ":" + minutes + ":" + seconds + " " + timePeriod;
+    container.textContent = time;
+  }
+  
+  updateTime();
+  setInterval(updateTime, 1000);
+  
